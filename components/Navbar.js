6dfd736dbb0d7e3b2691b5a5fcb37e74cbf88ab5
thephotogenicbug <navinkumarachari@gmail.com>
2021-09-02 13:25:57 +0530

    NextJS: First Next Project with fetch and map function and dynamic routes

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import Image from 'next/image'
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div className="logo">
+              <Image src="/science.png" width={50} height={50} />
+            </div>
+           <Link href="/"><a>Home</a></Link> 
+           <Link href="/about"><a>About</a></Link>
+           <Link href="/ninjas"><a>Ninja Listing</a></Link>
+        </nav>
+     );
+}
+ 
+export default Navbar;
